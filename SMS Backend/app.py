67dbd1da2cf56e6bd0636920f97c49f751d75e40
Just-Iced <plunkettjustice@gmail.com>
2025-11-08 08:49:09 -0800

    Added message queries, data fetching from sql server, and a help message

--- SMS Backend/app.py
+++ SMS Backend/app.py
@@ -6,6 +6,13 @@ import os, psycopg, re
 
 load_dotenv()
 
+help_message = """
+                OPTIONS:\n
+                1. ? - Shows this help menu.\n
+                2. SHOW ALL - Displays the status of all trashcans.\n
+                3. SHOW {BUILDING} - Displays the status of all trashcans in that building.
+                """
+
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 
@@ -24,18 +31,44 @@ cur = db.cursor()
 
 cur.execute("SET search_path TO smart_trashcan")
 
+def format_sql(resp: MessagingResponse) -> None:
+    message = ""
+    for trashcan in cur.fetchall():
+        id = trashcan[0]
+        name = trashcan[1]
+        status = trashcan[3]
+        
+        message += f"{id}. Trashcan {name} is currently {status}.\n"
+        
+    resp.message(message)
+
 # Handles everything for receiving sms messages
 @app.route("/sms", methods=["GET", "POST"])
-def sms_reply():
+def sms_reply() -> str:
 
-    body = request.values.get('Body', None)
-
+    body = request.values.get('Body', None).lower().split()
     resp = MessagingResponse()
+    
+    match body[0]:
+        case "?":
+            resp.message(help_message)
+        case "show":
+            match body[1]:
+                case "all":
+                    cur.execute("SELECT * FROM trashcans")
+                    format_sql(resp)
+
+                case _:
+                    cur.execute(f"SELECT * FROM trashcans WHERE LOWER(location) LIKE '%{body[1]}%'")
+                    format_sql(resp)
+        case _:
+            resp.message("Sorry, I didn't understand that.\n" + help_message)
 
     return str(resp)
 
+
 @app.route("/data", methods={"GET", "POST"})
-def data_fetch():
+def data_fetch() -> str:
     body = request.get_json()
     
     id = re.escape(body['id'])
