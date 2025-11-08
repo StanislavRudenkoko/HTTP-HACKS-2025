2eaab5bc9918d350ca539996354a1f1951a6f224
Just-Iced <plunkettjustice@gmail.com>
2025-11-08 00:40:05 -0800

    Added sql access to the SMS backend

--- SMS Backend/app.py
+++ SMS Backend/app.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request
 from dotenv import load_dotenv
-import os
+import os, psycopg, re
 
 load_dotenv()
 
@@ -12,6 +12,19 @@ auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 app = Flask(__name__)
 
+db = psycopg.connect(
+    host =       os.environ["SQL_HOST"],
+    port =       os.environ["SQL_PORT"],
+    dbname =     'trash_db',
+    user =       os.environ["SQL_USER"],
+    password =   os.environ["SQL_PASSWORD"]
+)
+
+cur = db.cursor()
+
+cur.execute("SET search_path TO smart_trashcan")
+
+# Handles everything for receiving sms messages
 @app.route("/sms", methods=["GET", "POST"])
 def sms_reply():
 
@@ -19,11 +32,15 @@ def sms_reply():
 
     resp = MessagingResponse()
 
-    # if body == 'hello':
-    #     resp.message("Hi!")
-
-    # elif body == 'bye':
-    #     resp.message("Goodbye")
-    print(body)
     return str(resp)
 
+@app.route("/data", methods={"GET", "POST"})
+def data_fetch():
+    body = request.get_json()
+    
+    id = re.escape(body['id'])
+    status = re.escape(body['status'])
+    
+    cur.execute(f"UPDATE trashcans SET status='{status}', last_updated=NOW() WHERE id='{id}'")
+    
+    return "Data was succesfully stored"
